776cfc1eade9eefca8a922c4542770ed8a10dcab
Nitin-Lohumi <lohuminitin@gmail.com>
2025-08-31 20:26:27 +0530

    Add

--- src/utility/Socket_Io.js
+++ src/utility/Socket_Io.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socketClient = io("http://localhost:4000/rooms", { autoConnect: false });
+const socketClient = io("https://codepad-server.onrender.com/rooms", { autoConnect: false });
 export default socketClient;
 
 // https://codepad-server.onrender.com/rooms
